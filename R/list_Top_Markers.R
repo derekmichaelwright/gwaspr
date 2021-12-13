bf3df401288f15dc241bd7d5ea8d27eb525c0fd1
derekmichaelwright <derek.wright@usask.ca>
2021-12-13 16:46:18 -0600

    edit

--- R/list_Top_Markers.R
+++ R/list_Top_Markers.R
@@ -8,19 +8,19 @@
 #' @return Table of top results.
 #' @export
 
-list_Top_Markers <- function(x, chroms = 1:7, n = 1, threshold_filter = T) {
+list_Top_Markers <- function(x, chroms = 1:7, n = 1, threshold = NULL) {
   #
   threshold <- -log10(0.05 / (nrow(x)) )
   #
   x <- x %>% filter(Chromosome %in% chroms) %>%
     group_by(Chromosome) %>%
     top_n(., n = n, -log10(P.value)) %>%
-    mutate(NegLog10 = round(-log10(P.value),2)) %>%
-    arrange(Chromosome, rev(NegLog10)) %>%
+    mutate(`-log10(p)` = round(-log10(P.value),2)) %>%
+    arrange(Chromosome, rev(`-log10(p)`)) %>%
     as.data.frame() %>%
     select(SNP, CHR=Chromosome, POS=Position, NegLog10)
   #
-  if(threshold_filter == T) { x <- x %>% filter(NegLog10 > threshold) }
+  if(!is.null(threshold)) { x <- x %>% filter(`-log10(p)` > threshold) }
   #
   x
 }
