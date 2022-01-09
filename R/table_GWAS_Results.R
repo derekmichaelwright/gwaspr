9adf3da564465e55c57bd872b3649821db41bfaf
derekmichaelwright <derek.wright@usask.ca>
2022-01-09 15:48:23 -0600

    edit

--- R/table_GWAS_Results.R
+++ R/table_GWAS_Results.R
@@ -2,24 +2,29 @@
 #'
 #' Create a table of significant GWAS results.
 #' @param folder Folder containing GWAS results.
-#' @param file The file to read.
+#' @param files The files to read.
 #' @param threshold Significant threshold.
 #' @param sug.threshold Suggestive threshold.
 #' @param rowread Number of rows to read.
 #' @return A table of significant GWAS results.
 #' @export
 
-table_GWAS_Results <- function(folder, file,
+table_GWAS_Results <- function(folder, files,
                               threshold = 6, sug.threshold = NULL,
                               rowread = 2000) {
   #
-  trait <- substr(file, gregexpr("GAPIT.", file)[[1]][1]+6,
-                  gregexpr(".GWAS.Results.csv", file)[[1]][1]-1 )
-  model <- substr(trait, 1, gregexpr("\\.", trait)[[1]][1]-1 )
-  trait <- substr(trait, gregexpr("\\.", trait)[[1]][1]+1, nchar(trait) )
-  #
-  output <- read.csv(paste0(folder, file), nrows = rowread) %>%
-    mutate(`-log10(p)` = -log10(P.value))
+  output <- NULL
+  for(i in files){
+    trait <- substr(i, gregexpr("GAPIT.", i)[[1]][1]+6,
+                    gregexpr(".GWAS.Results.csv", i)[[1]][1]-1 )
+    model <- substr(trait, 1, gregexpr("\\.", trait)[[1]][1]-1 )
+    trait <- substr(trait, gregexpr("\\.", trait)[[1]][1]+1, nchar(trait) )
+    #
+    oi <- read.csv(paste0(folder, i), nrows = rowread) %>%
+      mutate(`-log10(p)` = -log10(P.value))
+    output <- bind_rows(output, oi)
+  }
+
   #
   if(sum(colnames(output)=="nobs")>0) { output <- select(output, -nobs) }
   #
@@ -31,5 +36,7 @@ table_GWAS_Results <- function(folder, file,
     mutate(Model = model,
            Trait = trait,
            Threshold = ifelse(`-log10(p)` < threshold, "Suggestive", "Significant"))
-  output
+  output %>% arrange(desc(`-log10(p)`)) %>%
+    select(SNP, Chromosome, Position, Model, Trait, P.value, `-log10(p)`, maf, Threshold,
+           FDR_Adjusted_P.values, effect, Rsquare.of.Model.without.SNP, Rsquare.of.Model.with.SNP)
 }
