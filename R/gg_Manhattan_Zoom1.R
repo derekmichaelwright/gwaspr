8988b8d82bc4e768c130c3aca52dcd74679c53dc
derekmichaelwright <derek.wright@usask.ca>
2026-06-01 09:02:13 -0500

    edit

---
+++ R/gg_Manhattan_Zoom1.R
@@ -0,0 +1,193 @@
+#' gg_Manhattan_Zoom1
+#'
+#' Creates a manhattan plot zoomed in to a particular region.
+#' @param folder Folder containing GWAS results.
+#' @param traits The traits to read.
+#' @param title A title for the plot.
+#' @param chrom Chromosome to plot.
+#' @param pos1 Start position on chromosome.
+#' @param pos2 End position on chromosome.
+#' @param threshold Significant Threshold.
+#' @param sug.threshold Suggested threshold.
+#' @param markers Markers to be labelled.
+#' @param labels Labels to be used for markers.
+#' @param vlines Markers which will be used as a location for a vertical lines.
+#' @param vline.colors colors for each vertical line.
+#' @param vline.types lty for each vertical line.
+#' @param vline.legend Logical, whether or not to add a legend for the vlines.
+#' @param facet Logical, whether or not to produce a facetted or multi-model plot. Default is `facet = F`.
+#' @param addQQ Logical, whether or not to add a QQ plot
+#' @param pmax A max value for the y-axis.
+#' @param model Model to read.
+#' @param model.colors Colors for each model. Used if `facet = F`.
+#' @param highlight.sig Logical, whether or not to highlight significant associations with a black circle. Used if `facet = F`.
+#' @param sig.color Color for significant assoctiations.
+#' @param chrom.colors Colors for each chromosome. Used if `facet = T`.
+#' @param chrom.unit Unit for the x-axis. Can be one of c("kbp","100 kbp","Mbp","100 Mbp","Gbp").
+#' @param legend.rows Number of rows for the legend.
+#' @param plotHBPvalues Logical, should H.B.P.Values be uses.
+#' @param skyline Which skyline type to use. Can be "NYC" or "Kansas". If left NULL, it will use the highest P.value.
+#' @return A manhattan plot.
+#' @export
+
+gg_Manhattan_Zoom1 <- function(
+    folder = "GWAS_Results/",
+    traits = list_Traits(folder)[1],
+    title = NULL,
+    chrom, pos1, pos2,
+    threshold = NULL,
+    sug.threshold = NULL,
+    markers = NULL,
+    labels = markers,
+    vlines = markers,
+    vline.colors = rep("red", length(vlines)),
+    vline.types = rep(1, length(vlines)),
+    vline.legend = T,
+    pmax = NULL,
+    model =  "MLM",
+    model.colors = gwaspr_Colors,
+    facet = F,
+    highlight.sig = F,
+    sig.color = "red",
+    legend.rows = 1,
+    plotHBPvalues = F,
+    skyline = "Kansas"
+    ) {
+  #
+  # Read in files
+  #
+  fnames <- list_Result_Files(folder)
+  fnames <- fnames[grepl(paste0(model, ".", traits, collapse="|"), fnames)]
+  fnames <- fnames[grepl(paste0(traits, c(".csv","\\("), collapse="|"), fnames)]
+  #
+  if(!is.null(skyline)) {
+    if(skyline == "NYC") { fnames <- fnames[!grepl("\\(Kansas\\)", fnames)] }
+    if(skyline == "Kansas") {
+      fnames <- fnames[!grepl("\\(NYC\\)&FarmCPU", fnames)]
+      fnames <- fnames[!grepl("\\(NYC\\)&BLINK", fnames)]
+    }
+  }
+  #
+  xx <- NULL
+  #
+  for(i in fnames) {
+    trt <- substr(i, gregexpr("GWAS_Results", i)[[1]][1]+13,
+                  gregexpr(".csv", i)[[1]][1]-1 )
+    trt <- gsub("\\(Kansas\\)|\\(NYC\\)", "", trt)
+    mod <- substr(trt, 1, gregexpr("\\.", trt)[[1]][1] - 1)
+    trt <- substr(trt, gregexpr("\\.", trt)[[1]][1]+1, nchar(trt))
+    sky <- substr(i, gregexpr("\\(", i)[[1]][1] + 1, gregexpr("\\)", i)[[1]][1] - 1)
+    #
+    xi <- read.csv(paste0(folder, i))
+    if(sum(colnames(xi)=="nobs")>0) { xi <- dplyr::select(xi, -nobs) }
+    xi <- xi %>% mutate(Model = mod, Trait = trt, Type = sky, negLog10_P = -log10(P.value))
+    xx <- bind_rows(xx, xi)
+  }
+  #
+  xx <- xx %>% filter(P.value > 0) %>%
+    arrange(desc(P.value)) %>% filter(!duplicated(paste(SNP, Model)))
+  #
+  # Prep data
+  #
+  xx <- xx %>%
+    mutate(Model = factor(Model, levels = model)) %>%
+    filter(Chr == chrom, Pos > pos1, Pos < pos2, !is.na(Model))
+  #
+  model <- unique(xx$Model)
+  if(paste(model,collapse=",") != "MLM,MLMM,FarmCPU,BLINK,GLM,CMLM,SUPER" &
+     paste(model.colors,collapse=",") == "darkgreen,darkred,darkorange3,steelblue,darkorchid4,burlywood4,darkseagreen4") {
+    model.colors <- model.colors[c("MLM", "MLMM", "FarmCPU", "BLINK", "GLM", "CMLM", "SUPER") %in% model]
+  }
+  #
+  if(is.null(threshold)) {
+    threshold <- -log10(0.05/nrow(xi))
+  }
+  #
+  if(!is.null(pmax)) {
+    xx <- xx %>%
+      mutate(negLog10_P = ifelse(negLog10_P > pmax, pmax, negLog10_P))
+  }
+  #
+  xx <- xx %>%
+    mutate(Sig.level = ifelse(negLog10_P >= threshold, "Sig","Not Sig"))
+  if(!is.null(sug.threshold)) {
+    xx <- xx %>%
+      mutate(Sig.level = ifelse(negLog10_P < threshold & negLog10_P >= sug.threshold, "Sug", Sig.level))
+  }
+  #
+  if(plotHBPvalues == T) {
+    xx <- xx %>% mutate(Pvalue = -log10(H.B.P.Value))
+  } else {
+    xx <- xx %>% mutate(Pvalue = negLog10_P)
+  }
+  #
+  x2 <- xx %>% filter(negLog10_P >= threshold)
+  x3 <- xx %>% filter(SNP %in% markers) %>%
+    mutate(Label = plyr::mapvalues(SNP, markers, labels))
+  x4 <- xx %>% filter(SNP %in% vlines) %>%
+    filter(!duplicated(paste(SNP, Model)))
+  #
+  # Start Plots
+  #
+  mp <- ggplot(xx, aes(x = Pos / 1000000, y = Pvalue))
+  #
+  # Add vlines
+  #
+  if(!is.null(vlines)) {
+    mp <- mp +
+      geom_vline(data = x4, aes(xintercept = Pos / 1000000, color = SNP),
+                 alpha = 0.5) +
+      scale_color_manual(name = NULL, values = vline.colors)
+  }
+  #
+  # Add threshold lines
+  #
+  if(is.null(pmax)) { pmax <- max(xx$negLog10_P) }
+  mp <- mp +
+    geom_hline(yintercept = threshold, color = "red", alpha = 0.8, linewidth = 0.5) +
+    geom_hline(yintercept = sug.threshold, color = "blue", alpha = 0.8, linewidth = 0.5) +
+    scale_y_continuous(limits = c(0, (pmax+pmax*0.03)), expand = c(0,0))
+  #
+  # Plot the Rest
+  #
+  mp <- mp +
+    geom_hline(yintercept = threshold, color = "red", alpha = 0.6) +
+    geom_point(aes(fill = Model, size = Sig.level, key1 = SNP),
+               pch = 21, color = alpha("white",0)) +
+    scale_size_manual(name = NULL, values = c(1,2,1.5), guide = "none") +
+    guides(color = guide_legend(nrow = legend.rows, byrow = T, override.aes = list(alpha = 1))) +
+    theme_gwaspr(legend.position = "bottom",
+                 axis.title.y = element_markdown()) +
+    labs(title = title, y = "-log<sub>10</sub>(*p*)", x = "Mbp")
+  #
+  # Add Marker labels
+  #
+  if(!is.null(markers)) {
+    mp <- mp + geom_text_repel(data = x3, aes(label = Label), size = 2)
+  }
+  #
+  # Facet plot
+  #
+  if(facet == T) {
+    mp <- mp + facet_grid(Trait ~ Chr, scales = "free") +
+      geom_point(data = x3, aes(fill = Model, size = Sig.level), color = sig.color, pch = 21) +
+      scale_fill_manual(name = NULL, values = alpha(model.colors,0.8), guide = "none")
+  } else {
+    mp <- mp +
+      scale_fill_manual(name = NULL, values = alpha(model.colors,0.8)) +
+      guides(fill = guide_legend(nrow = legend.rows, override.aes = list(size = 2)))
+    if(highlight.sig == T) {
+      mp <- mp + geom_point(data = x2, fill = alpha("white", 0), pch = 21, size = 1.25)
+    }
+  }
+  mp
+}
+
+#folder = "GWAS_Results/"; trait = "Stem.Blight_Ba16"; title = trait #trait = list_Traits(folder)[1]
+#chrom = 4; pos1 = 400000000; pos2 = 500000000; threshold = NULL; sug.threshold = NULL
+#markers = NULL; labels = markers; vlines = markers
+#vline.colors = rep("red", length(vlines)); vline.types = rep(1, length(vlines))
+#vline.legend = T; models = c("MLM", "FarmCPU", "BLINK", "MLMM", "GLM", "CMLM", "SUPER")
+#model.colors = c("darkgreen", "darkorange3", "steelblue", "darkred", "darkorchid4", "burlywood4", "darkseagreen4")
+#facet = F; highlight.sig = F; sig.color = "red"; legend.rows = 1
+#plotHBPvalues = F; skyline = NULL
