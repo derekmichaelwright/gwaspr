39de8468432ea277631c8d924bbd6d9d4357c5f4
derekmichaelwright <derek.wright@usask.ca>
2026-07-26 22:32:43 -0500

    edit

--- R/mv.R
+++ R/mv.R
@@ -9,23 +9,5 @@
 #' @export
 
 mv <- function (x, from, to, warn_missing = TRUE) {
-  if (length(from) != length(to)) {
-    stop("`from` and `to` vectors are not the same length.")
-  }
-  if (!is.atomic(x) && !is.null(x)) {
-    stop("`x` must be an atomic vector or NULL.")
-  }
-  if (is.factor(x)) {
-    levels(x) <- mapvalues(levels(x), from, to, warn_missing)
-    return(x)
-  }
-  mapidx <- match(x, from)
-  mapidxNA <- is.na(mapidx)
-  from_found <- sort(unique(mapidx))
-  if (warn_missing && length(from_found) != length(from)) {
-    message("The following `from` values were not present in `x`: ",
-            paste(from[!(1:length(from) %in% from_found)], collapse = ", "))
-  }
-  x[!mapidxNA] <- to[mapidx[!mapidxNA]]
-  x
+  plyr::mapvalues(x = x, from = from, to = to, warn_missing = warn_missing)
 }
